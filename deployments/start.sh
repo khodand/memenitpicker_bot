9447579500a198580433e3078fb49503185158d3
Andrei Khodko <andrei.khodko@gmail.com>
2024-07-21 05:36:26 +0500

    importer

--- deployments/start.sh
+++ deployments/start.sh
@@ -2,12 +2,12 @@
 
 cd "$(dirname "$0")" || exit
 
-docker-compose down
-docker-compose pull
+docker compose down
+docker compose pull
 
 mkdir -p logs
 touch bot.log  # Creates an empty bot.log if it doesn't exist
 
 cp bot.log "logs/meme_bot_$(date +%F).log"
-docker-compose up -d
-nohup docker-compose logs -f > bot.log 2>&1 &
+docker compose up -d
+nohup docker compose logs -f > bot.log 2>&1 &
